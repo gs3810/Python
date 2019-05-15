b317fdfa782d4eecf8d96d7bfbf313da1e6e9dbd
gs3810 <46571277+gs3810@users.noreply.github.com>
2019-05-15 10:59:19 +0530

    Update Word Grams.py

--- NLP/Word Analysis/Word Grams.py
+++ NLP/Word Analysis/Word Grams.py
@@ -23,8 +23,8 @@ def clean_text(df, nlp):
         df['Headline'][i] = text
     return df
 
-def get_top_n2_words(corpus, n=None):
-    vec1 = CountVectorizer(ngram_range=(2,2),  
+def get_top_n2_words(corpus, n_w, n=None):
+    vec1 = CountVectorizer(ngram_range=(n_w,n_w),  
             max_features=2000).fit(corpus)
     bag_of_words = vec1.transform(corpus)
     sum_words = bag_of_words.sum(axis=0) 
@@ -66,12 +66,24 @@ stop_words = ["say","is", "to", "result", "large", "also", "iv", "one", "two", "
 cv = CountVectorizer(max_df=0.8,stop_words=stop_words, max_features=10000, ngram_range=(1,3))
 X = cv.fit_transform(corpus)
 
-top2_words = get_top_n2_words(corpus, n=20)
-top2_df = pd.DataFrame(top2_words)
-top2_df.columns=["Bi-gram", "Freq"]
-top2_df['Bi-gram'] = top2_df['Bi-gram'].str.title()
-print(top2_df)
+top_words = get_top_n2_words(corpus, n_w=5, n=40)
+top_df = pd.DataFrame(top_words)
+top_df.columns=["Bi-gram", "Freq"]
+top_df['Bi-gram'] = top_df['Bi-gram'].str.title()
+print(top_df)
 
-sns.set(rc={'figure.figsize':(13,8)})
-g = sns.barplot(x="Bi-gram", y="Freq", data=top2_df)
-g.set_xticklabels(g.get_xticklabels(), rotation=30)
+sns.set(rc={'figure.figsize':(17,8)})
+g = sns.barplot(x="Bi-gram", y="Freq", data=top_df)
+g.set_xticklabels(g.get_xticklabels(), rotation=80)
+
+# build a similarity index
+sim_df = pd.DataFrame()
+sim_df = sim_df.append([0])
+
+for i in range(0,top_df.shape[0]-1):
+    sim_df = sim_df.append([nlp(top_df['Bi-gram'][i]).similarity(nlp(top_df['Bi-gram'][i+1]))])
+sim_df = sim_df.reset_index(drop=True)
+sim_df.columns = ['Seq. Similarity']
+ 
+top_df = pd.concat([top_df, sim_df], axis=1)
+
