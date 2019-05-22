aa0bef84075879b2ae281e335a5d031278c48d3c
gs3810 <46571277+gs3810@users.noreply.github.com>
2019-05-22 11:55:26 +0530

    Update Headline Prediction test.py

--- NLP/Headline Clasf/Headline Prediction test.py
+++ NLP/Headline Clasf/Headline Prediction test.py
@@ -1,13 +1,12 @@
-"""https://www.kaggle.com/sudhirnl7/news-Headline-eda-spacy/notebook"""
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
 import spacy
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import BernoulliNB
 from sklearn.metrics import confusion_matrix, classification_report, roc_curve, auc
+#from imblearn.under_sampling import RandomUnderSampler
 import warnings
 import xlwings as xw
 
@@ -20,19 +19,15 @@ def clean_text(df, nlp):
     """
     for i in range(df.shape[0]):
         doc = nlp(df['Headline'][i])
-        # Word Tokenize
         #token = [w.text for w in doc]
         
         # Make Lower case
         # Remove Stop word, punctuation, digit and lemmatize
         text = [w.lemma_.lower().strip() for w in doc 
-               if not (w.is_stop |
-                    w.is_punct |
-                    w.is_digit)
+               if not (w.is_punct | w.is_digit) # w.is_stop
                ]
         text = " ".join(text)
         
-#        if i <5: print('Sentence:',i,text)
         df['Headline'][i] = text
     return df
 
@@ -45,38 +40,19 @@ def create_relv(thematic,string_df,name):
     relv_df.columns = [name]
     return relv_df  
 
-inputDF = pd.ExcelFile("News_dataset_test.xlsx")
+inputDF = pd.ExcelFile("News_Headlines.xlsx")
 tabnames = inputDF.sheet_names
 news = inputDF.parse(tabnames[0])
 
-# choose a column with certian number of words...
-news['num_words'] = news['Headline'].apply(lambda x: len(str(x).split()))
-text = news[news['num_words'] == 14]['Headline'].values
+## Word tokenize
+nlp = spacy.load('en_core_web_lg')
 
-# Word tokenize
-nlp = spacy.load('en_core_web_sm')
-doc = nlp(text[0])                  # select first element
-
-df = pd.DataFrame(
-{
-    'token': [w.text for w in doc],
-    'lemma':[w.lemma_ for w in doc],
-    'POS': [w.pos_ for w in doc],
-    'TAG': [w.tag_ for w in doc],
-    'DEP': [w.dep_ for w in doc],
-    'is_stopword': [w.is_stop for w in doc],
-    'is_punctuation': [w.is_punct for w in doc],
-    'is_digit': [w.is_digit for w in doc],
-})
-
-news_df = clean_text(news.iloc[0:100,:], nlp)
+news_df = clean_text(news.iloc[0:1000,:], nlp)
+news_df = news_df.drop_duplicates(subset='Headline', keep ='first').reset_index(drop=True)   # drop duplicates
 string_df = pd.DataFrame(news_df['Headline'])
 string_df.columns = ['Theme']
 
-# drop duplicates
-news_df = news_df.drop_duplicates(subset='Headline', keep ='first') 
-
-theme_words = ['trade, jobs, economy, fiscal','election, ballot','inflation, repo, interest rates']
+theme_words = ['trade, jobs, economy, fiscal budget surplus, pension, ministry PEMX','election, vote, sanctions, foreign minister','banks, issues ratings, inflation, repo, interest rates']
 theme_headers = ['Economy', 'Election','Rates']
 
 relv_df = pd.concat([string_df['Theme'], create_relv(theme_words[0],string_df,theme_headers[0])], axis=1)
@@ -88,7 +64,10 @@ sns.countplot(news_df['Relevance'])
 tf = TfidfVectorizer(analyzer='word',ngram_range=(1,3),max_features=50)
 X = tf.fit_transform(news_df['Headline'])
 
-"""Have to figure outputput of tf"""
+# convert to array
+X = pd.DataFrame(X.toarray())
+X_add = relv_df.iloc[:,1:4]
+X = pd.concat([X, X_add],axis=1).values
 
 y = news_df['Relevance']
 X_train, X_valid, y_train, y_valid = train_test_split(X,y, test_size=0.3, random_state=seed)
@@ -102,6 +81,14 @@ y_valid = y_valid.values
 print('Confusion matrix\n',confusion_matrix(y_valid,pred))
 print('Classification_report\n',classification_report(y_valid,pred))
 
+comp_out = pd.DataFrame([pred, y_valid]).T
+comp_out = pd.concat([relv_df.iloc[X_train.shape[0]:,0:4].reset_index(drop=True), comp_out], axis=1, ignore_index= True)
+comp_out.columns = ['News','Econ','Elc','Rates','Pred','Actual']
 
+# open output excel
+wb = xw.Book('Output.xlsx')
+sht = wb.sheets[0]
+sht.range('A1').value = pd.DataFrame(index=np.full(1000, np.nan), columns=np.full(30, np.nan))
+sht.range('A1').value = comp_out
 
 
