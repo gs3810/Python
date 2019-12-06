a69d332d758bda2be1a7c8e05fdeb7cfb4816661
gs3810 <46571277+gs3810@users.noreply.github.com>
2019-12-06 17:34:39 +0530

    Update Keras_model.py

--- Image_recog/Keras_model.py
+++ Image_recog/Keras_model.py
@@ -3,21 +3,13 @@ import os
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-
-model = tf.keras.Sequential([
-  hub.KerasLayer("https://tfhub.dev/google/tf2-preview/mobilenet_v2/feature_vector/4", 
-                 output_shape=[1280],
-                 trainable=False),
-  tf.keras.layers.Dropout(0.4),
-  tf.keras.layers.Dense(train_generator.num_classes, activation='softmax')
-])
+import tensorflow_hub as hub
 
 # Get Flowers dataset
 data_root = tf.keras.utils.get_file(
   'flower_photos','https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz',
    untar=True)
  
-
  # Create data generator for training and validation
 IMAGE_SHAPE = (224, 224)
 TRAINING_DATA_DIR = str(data_root)
@@ -33,3 +25,29 @@ valid_generator = valid_datagen.flow_from_directory(
 
 train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(**datagen_kwargs)
 train_generator = train_datagen.flow_from_directory(TRAINING_DATA_DIR,subset="training",shuffle=True,target_size=IMAGE_SHAPE)
+
+model = tf.keras.Sequential([
+  hub.KerasLayer("https://tfhub.dev/google/tf2-preview/mobilenet_v2/feature_vector/4", 
+                 output_shape=[1280],
+                 trainable=False),
+  tf.keras.layers.Dropout(0.4),
+  tf.keras.layers.Dense(train_generator.num_classes, activation='softmax')
+])
+
+model.compile(loss='categorical_crossentropy', optimizer='adam',metrics=['accuracy'])  
+  
+steps_per_epoch = np.ceil(train_generator.samples/train_generator.batch_size)
+val_steps_per_epoch = np.ceil(valid_generator.samples/valid_generator.batch_size)
+
+hist = model.fit(
+    train_generator, 
+    epochs=10,
+    verbose=1,
+    steps_per_epoch=steps_per_epoch,
+    validation_data=valid_generator,
+    validation_steps=val_steps_per_epoch).history
+
+# Measure accuracy and loss after training
+final_loss, final_accuracy = model.evaluate(valid_generator, steps = val_steps_per_epoch)
+print("Final loss: {:.2f}".format(final_loss))
+print("Final accuracy: {:.2f}%".format(final_accuracy * 100))
