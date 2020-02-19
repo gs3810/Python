38366975d33a0af5b4b37b2f7823dfefd55bbab1
gs3810 <46571277+gs3810@users.noreply.github.com>
2020-02-19 15:50:26 +0530

    Update tr_predict_ser.py

--- Image_recog/Conv_lite/tr_predict_ser.py
+++ Image_recog/Conv_lite/tr_predict_ser.py
@@ -5,6 +5,8 @@ import tensorflow as tf
 from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
 from keras.applications import imagenet_utils
+import glob
+import imageio
 
 def read_image(imagePath):
 
@@ -13,14 +15,16 @@ def read_image(imagePath):
     image = np.expand_dims(image, axis=0)
     return image
 
-saved_model_dir = 'models/gab4sig_model'
-model = tf.keras.models.load_model('models/gab4sig_model',compile=True) 
+saved_model_dir = 'model'
+model = tf.keras.models.load_model(saved_model_dir,compile=True) 
 
 # read in labels
-labels = pd.read_excel('models/Labels.xlsx').iloc[:,1:]
+labels = pd.read_excel('labels/Labels.xlsx').iloc[:,1:]
 
-# runinference
-image = read_image('test/test_metal_1.jpg')
-tf_pred = pd.DataFrame(model.predict(image), columns=labels.columns).round(2)
-
-
+tf_out =[]
+for file in glob.glob('test/*.jpg'):
+    image = read_image(file)
+    tf_pred = pd.DataFrame(model.predict(image), columns=labels.columns).round(2)
+    tf_out.append(tf_pred)
+    
+# pd.concat[] convert to df and out to excel
